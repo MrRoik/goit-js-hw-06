dd2be0f5acc8e5db5d1564b95fda19410228a888
MrRoik <138239220+MrRoik@users.noreply.github.com>
2023-11-06 19:55:24 +0200

    task 9 - js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,28 @@
+/*
+Напиши скрипт, який змінює кольори фону елемента <body>
+через інлайн-стиль по кліку на button.change-color і виводить значення кольору в span.color.
+
+*/
+const clickButton = document.querySelector(".change-color")
+const spanColor = document.querySelector(".color")
+const bodyColor = document.body;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+/*function getRandomHexButColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}*/
+
+const changeColor = () => {
+  spanColor.textContent = getRandomHexColor();
+  bodyColor.style.backgroundColor = spanColor.textContent;
+  //clickButton.style.backgroundColor = getRandomHexButColor();
+}
+
+clickButton.addEventListener("click", changeColor);
